c4407845f7b58a286f9083f177f353857485d0ca
Alvin Emir Gil Saldaña <alvingil86@gmail.com>
2026-09-06 11:44:38 -0500

    actualizando repositorio

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,10 @@
 <?php
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit;
-}
+/**
+ * Uninstall handler for WordPress Chapters Logos.
+ *
+ * @package Videos_Chapters_Logos
+ */
 
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
